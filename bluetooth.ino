732e1efc5803ad68c6daabd9487fb4d5409e1b72
Ziad <111704944+zizoisprogramming@users.noreply.github.com>
2025-04-29 02:49:59 +0300

    Create bluetooth.ino

---
+++ bluetooth.ino
@@ -0,0 +1,23 @@
+
+#include<avr/io.h>
+#include<avr/interrupt.h>
+#include<util/delay.h>
+
+
+// Connect a resistor (e.g., 10kΩ) between PD0 and GND physically on the circuit.
+
+#define RX_PIN  PD0
+#define TX_PIN  PD1
+
+void initialize_bluetooth() {
+    DDRD &= ~(1 << RX_PIN);
+    DDRD |= (1 << TX_PIN);
+}
+
+void wait_for_bluetooth() {
+    while (!(PIND & (1 << RX_PIN)));
+}
+
+int main() {
+    wait_for_bluetooth();
+}
